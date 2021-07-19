9ad1481d48f4c9bb40e3222dd91f82ca03b389e8
Purinut <reawjangpurinut@gmail.com>
2021-07-19 16:04:56 +0700

    initiate making quiz page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,29 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from "react-router-dom";
 
 import Home from "./Home";
+import CreateQuiz from "./CreateQuiz";
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        
+        <Switch>
+          <Route path="/create-quiz">
+            <CreateQuiz />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+
+        </Switch>
+
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/CreateQuiz.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function CreateQuiz(){
+    return (
+        <div className="quizPanel">
+            <div className="title">
+                <h3>Quiz Title</h3>
+            </div>
+        </div>
+    )
+}
+
+export default CreateQuiz;
